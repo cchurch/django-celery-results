bc70ad18e394b8cf41cd8eaa7168b1233943f184
Chris Church <chris@ninemoreminutes.com>
2018-06-12 22:48:07 -0400

    Prevent adding/changing task results in admin.

--- django_celery_results/admin.py
+++ django_celery_results/admin.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import TaskResult
 
@@ -12,7 +13,9 @@ class TaskResultAdmin(admin.ModelAdmin):
     model = TaskResult
     list_display = ('task_name', 'task_id', 'date_done', 'status')
     list_filter = ('status', 'date_done')
-    readonly_fields = ('date_done', 'result', 'hidden', 'meta')
+    readonly_fields = ('task_name', 'task_id', 'status', 'content_type',
+                       'content_encoding', 'task_args', 'task_kwargs',
+                       'traceback', 'date_done', 'result', 'hidden', 'meta')
     fieldsets = (
         (None, {
             'fields': (
@@ -24,14 +27,14 @@ class TaskResultAdmin(admin.ModelAdmin):
             ),
             'classes': ('extrapretty', 'wide')
         }),
-        ('Parameters', {
+        (_('Parameters'), {
             'fields': (
                 'task_args',
                 'task_kwargs',
             ),
             'classes': ('extrapretty', 'wide')
         }),
-        ('Result', {
+        (_('Result'), {
             'fields': (
                 'result',
                 'date_done',
@@ -42,6 +45,15 @@ class TaskResultAdmin(admin.ModelAdmin):
             'classes': ('extrapretty', 'wide')
         }),
     )
+
+    def has_add_permission(self, request):
+        return False
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context.setdefault('show_save', False)
+        extra_context.setdefault('show_save_and_continue', False)
+        return super(TaskResultAdmin, self).change_view(request, object_id, form_url, extra_context)
 
 
 admin.site.register(TaskResult, TaskResultAdmin)
